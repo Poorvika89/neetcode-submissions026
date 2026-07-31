4f80bfe398ac51acec559b29d067392536211def
Poorvika89 <03poorvi10@gmail.com>
2026-07-31 15:04:54 +0530

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int size=nums.length;
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[i]==nums[j])
+                 return true;
+            }
+        }
+       
+      return false;  
+    }
+}
